9e8c76054e1dd851d1f7514276e374488d875660
Mezriss <mezriss@gmail.com>
2016-11-12 20:02:09 +0200

    From private methothods to extra private

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
+const _listenerList = new WeakMap(); 
+
+function _listener (event) {
+	if (!Array.isArray(_listenerList.get(this)[event.type])) {
+		return;
+	}
+	_listenerList.get(this)[event.type].forEach(el => {
+		let insideEvent = false;
+
+		for (let i = 0; i < el.elements.length && !insideEvent; i += 1) {
+			let nodes = typeof el.elements[i] === 'string' ? document.querySelectorAll(el.elements[i]) : el.elements[i];
+			for (let j = 0; j < nodes.length && !insideEvent; j += 1) {
+				insideEvent = nodes[j].contains(event.target);
+			}
+		}
+
+		if (!insideEvent) {
+			el.cb(event);
+			if (el.once) {
+				this.off(el.cb);
+			}
+		}
+	});
+};
+
 export default class OutsideEvent {
 	constructor() {
-		this._listeners = {};
+		_listenerList.set(this, {});
 		if (!document.body) {
 			throw new Error(`Don't initialize outsideEvent service before <body> is rendered`);
 		}
 	}
 
-	_listener(event) {
-		if (!Array.isArray(this._listeners[event.type])) {
-			return;
-		}
-		this._listeners[event.type].forEach(el => {
-			let insideEvent = false;
-
-			for (let i = 0; i < el.elements.length && !insideEvent; i += 1) {
-				let nodes = typeof el.elements[i] === 'string' ? document.querySelectorAll(el.elements[i]) : el.elements[i];
-				for (let j = 0; j < nodes.length && !insideEvent; j += 1) {					
-					insideEvent = nodes[j].contains(event.target);
-				}
-			}
-
-			if (!insideEvent) {
-				el.cb(event);
-				if (el.once) {
-					this.off(el.cb);
-				}
-			}
-		});
-	}
 	/*
 		@events - string with event type or list of event types (with space as delimiter)
 		@elements - selector string, or node, or nodeList or array of any combination of these
@@ -42,33 +45,35 @@ export default class OutsideEvent {
 		}
 
 		elements.forEach((el, i) => {
-				if (el instanceof HTMLElement) {
-					elements[i] = [el];
-				}
+			if (el instanceof HTMLElement) {
+				elements[i] = [el];
 			}
+		}
 		);
 
 		events.forEach(event => {
-			if (!this._listeners[event]) {
-				document.addEventListener(event, this._listener.bind(this));
-				this._listeners[event] = [];
+			if (!_listenerList.get(this)[event]) {
+				document.addEventListener(event, _listener.bind(this));
+				_listenerList.get(this)[event] = [];
 			}
-			this._listeners[event].push({elements, cb, once});
+			_listenerList.get(this)[event].push({ elements, cb, once });
 		});
 	}
+
 	once(events, elements, cb) {
 		this.on(events, elements, cb, true);
 	}
+	
 	off(cb) {
-		Object.keys(this._listeners).forEach(eventType => {
-			const index = this._listeners[eventType].findIndex(el => el.cb === cb);
+		Object.keys(_listenerList.get(this)).forEach(eventType => {
+			const index = _listenerList.get(this)[eventType].findIndex(el => el.cb === cb);
 			if (index < 0) {
 				return;
 			}
-			this._listeners[eventType].splice(index, 1);
-			if (!this._listeners[eventType].length) {
-				delete this._listeners[eventType];
-				document.removeEventListener(eventType, this._listener);
+			_listenerList.get(this)[eventType].splice(index, 1);
+			if (!_listenerList.get(this)[eventType].length) {
+				delete _listenerList.get(this)[eventType];
+				document.removeEventListener(eventType, _listener);
 			}
 		});
 	}
