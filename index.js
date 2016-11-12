0d37a9f92168a38939ef03706ab1b8d6fd311036
Mezriss <mezriss@gmail.com>
2016-11-12 19:15:33 +0200

    Additional API, streamlined code

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-
 export default class OutsideEvent {
 	constructor() {
 		this._listeners = {};
-		this._matches = (el, selector) => 
-			(el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector).call(el, selector);
-		this._body = document.querySelector('body');
-		if (!this._body) {
+		if (!document.body) {
 			throw new Error(`Don't initialize outsideEvent service before <body> is rendered`);
 		}
-	}
-
-	_checkParentsForNode(node, target) {
-		if (!target || target === this._body) {
-			return false;
-		}
-
-		return (typeof node === 'string' ? this._matches(target, node) : node === target) ? 
-			true : this._checkParentsForNode(node, target && target.parentNode);
 	}
 
 	_listener(event) {
@@ -27,32 +14,35 @@ export default class OutsideEvent {
 			let insideEvent = false;
 
 			for (let i = 0; i < el.elements.length && !insideEvent; i += 1) {
-				for (let j = 0; j < el.elements[i].length && !insideEvent; j += 1) {
-					insideEvent = this._checkParentsForNode(el.elements[i][j], event.target);
+				let nodes = typeof el.elements[i] === 'string' ? document.querySelectorAll(el.elements[i]) : el.elements[i];
+				for (let j = 0; j < nodes.length && !insideEvent; j += 1) {					
+					insideEvent = nodes[j].contains(event.target);
 				}
 			}
 
 			if (!insideEvent) {
 				el.cb(event);
+				if (el.once) {
+					this.off(el.cb);
+				}
 			}
 		});
 	}
 	/*
-		@events - string or array of strings
+		@events - string with event type or list of event types (with space as delimiter)
 		@elements - selector string, or node, or nodeList or array of any combination of these
 		@cb - this one should be obvious; don't forget to save reference somewhere for unsubscribing
+		@once - run only once
 	 */
-	on(events, elements, cb) {
-		if (!Array.isArray(events)) {
-			events = [events];
-		}
+	on(events, elements, cb, once) {
+		events = events.split(' ');
 
 		if (!Array.isArray(elements)) {
 			elements = [elements];
 		}
 
 		elements.forEach((el, i) => {
-				if (el instanceof HTMLElement || typeof el === 'string') {
+				if (el instanceof HTMLElement) {
 					elements[i] = [el];
 				}
 			}
@@ -60,11 +50,14 @@ export default class OutsideEvent {
 
 		events.forEach(event => {
 			if (!this._listeners[event]) {
-				this._body.addEventListener(event, this._listener.bind(this));
+				document.addEventListener(event, this._listener.bind(this));
 				this._listeners[event] = [];
 			}
-			this._listeners[event].push({elements: elements, cb: cb});
+			this._listeners[event].push({elements, cb, once});
 		});
+	}
+	once(events, elements, cb) {
+		this.on(events, elements, cb, true);
 	}
 	off(cb) {
 		Object.keys(this._listeners).forEach(eventType => {
@@ -75,7 +68,7 @@ export default class OutsideEvent {
 			this._listeners[eventType].splice(index, 1);
 			if (!this._listeners[eventType].length) {
 				delete this._listeners[eventType];
-				this._body.removeEventListener(eventType, this._listener);
+				document.removeEventListener(eventType, this._listener);
 			}
 		});
 	}
